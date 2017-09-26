3d4dc2663e6b590e3d3f17a23b23d40c22cb6cab
Matt Waltz <matthewwaltzis@gmail.com>
2017-09-26 11:32:53 -0700

    Clear out old items on exit

--- src/routines/exit.asm
+++ src/routines/exit.asm
@@ -3,19 +3,19 @@ FullExit:
 	call	_boot_ClearVRAM
 	ld	a,$2D
 	ld	(mpLcdCtrl),a                   ; Set LCD to 16bpp
-	set	graphdraw,(iy+graphFlags)
 	res	useTokensInString,(iy+clockFlags)
 	res	onInterrupt,(iy+onFlags)        ; [ON] break error destroyed
-	call	_ClrTxtShd                      ; clear text shadow
 	call	_DrawStatusBar
 	call	_ClrParserHook
 	res	apdWarmStart, (iy + apdFlags)
 	ld	a,(AutoBackup)
 	or	a,a
 	call	nz,ClearOldBackup
-	im	1
-	ei
-	jp	_JForceCmdNoChar                 ; clear the screen like a boss
+	ld	hl,WipeSafeRam_Start
+	ld	de,WipeSafeRam
+	ld	bc,WipeSafeRam_End-WipeSafeRam_Start
+	ldir
+	jp	WipeSafeRam
 
 ClearOldBackup:
 	di                                       ; let's do some crazy flash things so that way we can save the RAM state...
@@ -29,5 +29,19 @@ ClearOldBackup:
 	call.is	flock & $ffff
 
 	ret
+
+WipeSafeRam_Start:
+relocate(cursorimage)
+WipeSafeRam:
+	ld	hl,pixelShadow
+	ld	bc,69090
+	call	_MemClear
+	call	_ClrTxtShd                      ; clear text shadow
+	set	graphdraw,(iy+graphFlags)
+	im	1
+	ei
+	jp	_JForceCmdNoChar                 ; exit like a boss
+endrelocate()
+WipeSafeRam_End:
 
 #include "routines/ramquit.asm"
