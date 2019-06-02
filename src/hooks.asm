23eada33d98729eadbeff5bd11b606a6f106b3a5
mateoconlechuga <matthewwaltzis@gmail.com>
2019-06-01 18:01:15 -0600

    add jump to label in editor

--- src/hooks.asm
+++ src/hooks.asm
@@ -77,17 +77,273 @@ hook_get_key:
 	cp	a,ti.skGraph
 	jr	z,hook_show_labels
 	cp	a,ti.skPrgm
-	jr	z,hook_execute_cesium
+	jq	z,hook_execute_cesium
 	cp	a,ti.sk8
-	jr	z,hook_backup_ram
+	jq	z,hook_backup_ram
 	cp	a,ti.sk5
-	jr	z,hook_clear_backup
+	jq	z,hook_clear_backup
 	cp	a,ti.sk2
-	jr	z,hook_restore_ram
+	jq	z,hook_restore_ram
 	ret
 
+label_number     := ti.cursorImage + 3
+label_number_of_pages := label_number + 3
+label_page := label_number_of_pages + 3
+label_name       := label_page + 3
+
 hook_show_labels:
-	jr	hook_get_key_none
+	di
+	ld	a,(ti.cxCurApp)
+	cp	a,ti.cxPrgmEdit
+	jq	nz,hook_get_key_none
+
+	hook_strings.copy
+
+	call	ti.CursorOff
+
+	or	a,a
+	sbc	hl,hl
+	ld	(label_number),hl
+	ld	(label_page),hl
+	call	ti.ClrTxtShd
+	call	ti.BufToTop
+
+	call	.countlabels
+
+	ld	bc,0
+	ld	hl,(label_number)
+	add	hl,de
+	or	a,a
+	sbc	hl,de
+	jq	z,.movetolabel
+
+.getlabelloop:
+	call 	ti.ClrScrn
+	call	.drawlabels
+
+	ld	hl,(label_page)
+	ld	de,.current_page_string
+	inc	hl
+	call	helper_num_convert
+
+	ld	hl,.page_string
+	call	helper_vputs_toolbar
+
+.getkey:
+	di
+	call	ti.DisableAPD
+	call	ti.GetCSC
+	or	a,a
+	jr	z,.getkey
+	ld	bc,1
+	cp	a,ti.sk0
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk1
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk2
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk3
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk4
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk5
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk6
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk7
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk8
+	jr	z,.movetolabel
+	inc	c
+	cp	a,ti.sk9
+	jr	z,.movetolabel
+	cp	a,ti.skLeft
+	jq	z,.prevpage
+	cp	a,ti.skRight
+	jr	z,.nextpage
+	jr	.getkey
+
+.movetolabel:
+	ld	a,(ti.curRow)
+	cp	a,c
+	jr	z,.okay
+	jr	c,.getkey
+.okay:
+	call	.computepageoffsethl
+	add	hl,bc
+	push	hl
+	call	ti.BufToTop
+	pop	bc
+	call	.skiplabels
+	call	ti.BufLeft
+	call 	ti.ClrScrn
+	xor	a,a
+	ld	(ti.curCol),a
+	ld	(ti.curRow),a
+	ld	a,(ti.winTop)
+	or	a,a
+	jr	z,.incesiumeditor	; check if using cesium feature
+	ld	hl,.program_string
+	call	ti.PutS
+	ld	hl,ti.progToEdit
+	call	ti.PutS
+	call	ti.NewLine
+.incesiumeditor:
+	ld	a,':'
+	call	ti.PutMap
+	ld	a,1
+	ld	(ti.curCol),a
+	call	ti.DispEOW
+	call	ti.CursorOn
+	call	ti.DrawStatusBar
+	jq	hook_get_key_none
+
+.nextpage:
+	ld	hl,(label_page)
+	ld	de,(label_number_of_pages)
+	or	a,a
+	sbc	hl,de
+	add	hl,de
+	jr	z,.firstpage
+	inc	hl
+	jr	.setpage
+.firstpage:
+	or	a,a
+	sbc	hl,hl
+.setpage:
+	ld	(label_page),hl
+	jq	.getlabelloop
+.prevpage:
+	ld	hl,(label_page)
+	add	hl,de
+	or	a,a
+	sbc	hl,de
+	jr	z,.lastpage
+	dec	hl
+	jr	.setpage
+.lastpage:
+	ld	hl,(label_number_of_pages)
+	jr	.setpage
+
+.countlabels:
+	call	ti.BufToTop
+.loop:
+	call	ti.BufRight
+	jr	z,.getnumpages
+	ld	a,e
+	cp	a,ti.tLbl
+	jr	nz,.loop
+	ld	hl,(label_number)
+	inc	hl
+	ld	(label_number),hl
+	jr	.loop
+.getnumpages:
+	ld	hl,(label_number)
+	dec	hl
+	ld	a,10
+	call	ti.DivHLByA
+	ld	(label_number_of_pages),hl
+	inc	hl
+	ld	de,.total_page_string
+	jp	helper_num_convert
+
+.skiplabelsloop:
+	push	bc
+	call	ti.BufRight
+	pop	bc
+	ret	z
+	ld	a,e
+	cp	a,ti.tLbl
+	jr	nz,.skiplabelsloop
+	dec	bc
+.skiplabels:
+	sbc	hl,hl
+	adc	hl,bc
+	jr	nz,.skiplabelsloop
+	ret
+
+.drawlabels:
+	call	ti.BufToTop
+	xor	a,a
+	ld	(ti.curRow),a
+	ld	(ti.curCol),a
+	call	.computepageoffset
+	call	.skiplabels
+	ld	hl,label_name
+.parse_labels:
+	push	hl
+	call	ti.BufRight
+	pop	hl
+	ret	z
+	ld	a,e
+	cp	a,ti.tLbl
+	jr	nz,.parse_labels
+.add_label:
+	push	hl
+	call	ti.BufRight
+	pop	hl
+	jr	z,.added_label
+	ld	a,e
+	cp	a,ti.tColon
+	jr	z,.added_label
+	cp	a,ti.tEnter
+	jr	z,.added_label
+	ld	(hl),a
+	inc	hl
+	jr	.add_label
+.added_label:
+	xor	a,a
+	ld	(hl),a
+	ld	(ti.curCol),a
+	ld	a,(ti.curRow)
+	add	a,'0'
+	call	ti.PutC
+	ld	a,':'
+	call	ti.PutC
+	ld	hl,label_name
+	push	hl
+	call	ti.PutS
+	ld	a,(ti.curRow)
+	inc	a
+	ld	(ti.curRow),a
+	cp	a,10
+	pop	hl
+	ret	z
+	jr	.parse_labels
+
+.computepageoffsethl:
+	push	bc
+	call	.computepageoffset
+	pop	bc
+	ret
+.computepageoffset:
+	ld	hl,(label_page)
+	ld	bc,10
+	call	ti._imulu
+	push	hl
+	pop	bc
+	ret
+
+relocate hook_strings, ti.plotSScreen
+.program_string:
+	db	"PROGRAM:",0
+.page_string:
+	db	"Use <> to switch page:     <"
+.current_page_string:
+	db	"000"
+	db	" of "
+.total_page_string:
+	db	"000"
+	db	">",0
+end relocate
 
 hook_clear_backup:
 	call	flash_code_copy
@@ -120,17 +376,8 @@ hook_get_key_none:
 
 hook_backup_ram:
 	call	ti.os.ClearStatusBarLow
-	di
-	ld	de,$e71c
-	ld.sis	(ti.drawFGColor and $ffff),de
-	ld.sis	de,(ti.statusBarBGColor and $ffff)
-	ld.sis	(ti.drawBGColor and $ffff),de
-	ld	a,14
-	ld	(ti.penRow),a
-	ld	de,2
-	ld.sis	(ti.penCol and $ffff), de
 	ld	hl,string_ram_backup
-	call	ti.VPutS
+	call	helper_vputs_toolbar
 	call	flash_code_copy
 	call	flash_backup_ram
 	call	ti.DrawStatusBar
@@ -286,3 +533,37 @@ hook_home:
 	dl	$f8
 	dl	$f8
 	db	0
+
+helper_vputs_toolbar:
+	di
+	ld	a,$d0
+	ld	mb,a
+	ld	de,$e71c
+	ld.sis	(ti.drawFGColor and $ffff),de
+	ld.sis	de,(ti.statusBarBGColor and $ffff)
+	ld.sis	(ti.drawBGColor and $ffff),de
+	ld	a,14
+	ld	(ti.penRow),a
+	ld	de,2
+	ld.sis	(ti.penCol and $ffff), de
+	jp	ti.VPutS
+
+helper_num_convert:
+	ld	a,4
+.entry:
+	ld	bc,-100
+	call	.aqu
+	ld	c,-10
+	call	.aqu
+	ld	c,b
+.aqu:
+	ld	a,'0' - 1
+.under:
+	inc	a
+	add	hl,bc
+	jr	c,.under
+	sbc	hl,bc
+	ld	(de),a
+	inc	de
+	ret
+
