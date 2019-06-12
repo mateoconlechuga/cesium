bce4c9879befe7b2dd76f75b84c6122ccf555a29
mateoconlechuga <matthewwaltzis@gmail.com>
2019-06-11 18:44:14 -0600

    bump version

--- src/cesium.asm
+++ src/cesium.asm
@@ -4,7 +4,7 @@
 ; (c) 2015-2018 Matt "Mateoconlechuga" Waltz
 
 cesium_name := 'Cesium'
-cesium_version := '3.1.7'
+cesium_version := '3.1.8'
 cesium_copyright := '(C)  2015-2019 Matt Waltz'
 
 include 'include/macros.inc'
